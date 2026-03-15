c29b32a62443bf936608a26f8d4af3cfef1caf5f
Ricky <>
2026-03-15 20:32:07 +0700

    udp

--- apps/portal/next.config.mjs
+++ apps/portal/next.config.mjs
@@ -19,6 +19,11 @@ const nextConfig = {
         hostname: 'static-test.resdii.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '/**',
+      },
     ],
   },
 };
